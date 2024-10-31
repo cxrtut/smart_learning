195c0acc10811014c284a7ce4a2ec03be6cd750f
MFANAMKHULU <139135865+MFANAMKHULU@users.noreply.github.com>
2024-10-31 13:59:43 +0200

    Adding options to options screen

--- app/(dashboard)/subject/[id]/Homework.tsx
+++ app/(dashboard)/subject/[id]/Homework.tsx
@@ -1,14 +1,25 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import colors from '@/constants/colors'
+import { useLocalSearchParams } from 'expo-router';
+import { useOnboarding } from '@/context/onboardingContext';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import CustomHeader from '@/components/CustomHeader';
+import colors from '@/constants/colors';
 
-const Homework = () => {
+const Quiz = () => {
+  const {id} = useLocalSearchParams<{id:string}>();
+  const {activeSubject} = useOnboarding();
+
   return (
     <SafeAreaView style={{backgroundColor: colors.PRIMARY}} className='flex h-full w-full'>
-      <Text>Homework</Text>
+      <CustomHeader  
+          title='Homework'
+          subtitle={activeSubject?.subjectName}
+          showBackButton={true}
+          headerStyles='pr-3'
+      />
     </SafeAreaView>
   )
 }
 
-export default Homework
+export default Quiz
