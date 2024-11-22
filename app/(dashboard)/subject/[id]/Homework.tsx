1432afc87b4baa1eaa593c0752ac9fca278fe7c7
Machaba Rotshidzwa Kaizer <machabarotshidzwakaizer@gmail.com>
2024-11-22 13:58:28 +0200

    Update Homework.tsx and removing secrets

--- app/(dashboard)/subject/[id]/Homework.tsx
+++ app/(dashboard)/subject/[id]/Homework.tsx
@@ -1,117 +1,102 @@
-import { View,  ScrollView, FlatList, Text, KeyboardAvoidingView, Platform, TouchableOpacity, TextInput } from 'react-native'
-import React, { useEffect,useState } from 'react';
-import { useLocalSearchParams } from 'expo-router';
+import { View, Text, TextInput, TouchableOpacity, Alert, Switch } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Href, router, useLocalSearchParams, useRouter } from 'expo-router';
 import { useOnboarding } from '@/context/onboardingContext';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import CustomHeader from '@/components/CustomHeader';
 import colors from '@/constants/colors';
-import OpenAI from 'openai';
-
-
-
-interface Message{
-  id: string;
-  type: 'text';
-  content: string;
-  sender: 'user' | 'system';
-}
-
-const openai = new OpenAI({
-  apiKey: "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",
-})
+import { Image } from 'react-native';
+import { images } from '@/constants';
+import ChatInputSection from '@/components/ChatInputSection';
+import ReactNativeModal from 'react-native-modal';
+import { Camera, CameraPermissionStatus } from 'react-native-vision-camera';
 
 const Homework = () => {
-  const {id} = useLocalSearchParams<{id:string}>();
-  const {activeSubject} = useOnboarding();
+    const { media, type } = useLocalSearchParams();
+    const {activeSubject} = useOnboarding();
 
-  const [messages, setMessages] = useState<Message[]>([]);
-  const [inputText, setInputText] = useState('');
-  const [loading, setLoading] = useState(false);
+    const [isChatActive, setIsChatActive] = useState(false)
+    const [showRequestModal, setShowRequestModal] = useState(false)
 
-  const sendMessage = async () =>{
-    if(inputText.trim().length === 0) return;
+    const [cameraPermissionStatus, setCameraPermissionStatus] = 
+    React.useState<CameraPermissionStatus>("not-determined")
 
-    // add a message
-    const userMessage: Message = { id: Date.now().toString(), type: 'text', content: inputText, sender: 'user' };
-    setMessages((prev) => [...prev, userMessage]);
-    setInputText('');
-
-    // generate ai response
-    setLoading(true);
-    try{
-      const response = await openai.chat.completions.create({
-        model: 'gpt-3.5-turbo',
-        messages:[
-          { role: 'system', content: 'You are a helpful assistant.' },
-          ...messages.map((msg) => ({
-            role: msg.sender === 'user' ? 'user' : 'assistant',
-            content: msg.content,
-          })),
-          { role: 'user', content: inputText },
-        ],
-      });
-
-      const aiMessage: Message = {
-        id: Date.now().toString(),
-        type: 'text',
-        content: response.choices[0]?.message?.content || 'Sorry, I did not understand that.',
-        sender: 'system',
-      };
-      setMessages((prev) => [...prev, aiMessage]);
-    } catch (error){
-      console.error('Error generating AI response:', error);
-    } finally{
-      setLoading(false);
+    const requestCameraPermission = async () => {
+        const permissions = await Camera.requestCameraPermission()
+        setCameraPermissionStatus(permissions)
     }
-  };
+    
+    //Camera Permissions
+    const handleCameraPermission = async () => {
+            setShowRequestModal(false)
+            requestCameraPermission() 
+    }
 
-  const renderMessage = ({ item }: { item: Message }) => (
-    <View
-      className={`my-1 p-2 rounded-lg max-w-3/4 ${
-        item.sender === 'user' ? 'bg-blue-500 self-end' : 'bg-gray-300 self-start'
-      }`}
-    >
-      <Text className={item.sender === 'user' ? 'text-white' : 'text-black'}>
-        {item.content}
-      </Text>
-    </View>
-  );
+    const handleContinue = () => {
+        if(cameraPermissionStatus === "granted") {
+            router.push('/(dashboard)/subject/[id]/(homework)/Camera')
+        } else {
+            setShowRequestModal(true)
+        }
+    }
 
-  return (
-    <SafeAreaView style={{backgroundColor: colors.PRIMARY}} className='flex h-full w-full'>
-      <CustomHeader  
-          title='Homework'
-          subtitle={activeSubject?.subjectName}
-          showBackButton={true}
-          headerStyles='pr-3'
-      />
-        <KeyboardAvoidingView 
-            behavior={Platform.OS === 'ios' ? 'padding' : undefined} 
-            className = {'flex-1 bg-gray-100'}
-            keyboardVerticalOffset={100}>
 
-            <ScrollView>
-            <FlatList
-                className={'flex-1 p-8'}
-                data={messages}
-                renderItem={renderMessage}
-                keyExtractor={(item) => item.id}
-            />
-            </ScrollView>
-            <View className={`flex-row items-center border-t border-gray-300 p-2`}>
-                <TextInput
-                    className={`flex-1 border border-black-400 p-8 rounded-lg px-4 py-2`}
-                    placeholder="Ask me a question..."
-                    value={inputText}
-                    onChangeText={setInputText}
+    return (
+        <SafeAreaView style={{backgroundColor: colors.PRIMARY}} className='flex h-full w-full justify-between'>
+            <View className='flex h-[70%] w-full'>
+                <CustomHeader  
+                    title='Homework'
+                    subtitle={activeSubject?.subjectName}
+                    showBackButton={true}
+                    headerStyles='pr-3'
                 />
-                <TouchableOpacity onPress={sendMessage} className={`ml-2 bg-blue-500 p-3 rounded-full`}>
-                    <Text className={`text-white`}>Send</Text>
-                </TouchableOpacity>
+
+                {!isChatActive && <View className='flex items-center justify-center'>
+                    <Image
+                    source={images.onboarding13}
+                    style={{ width: 400, height: 650 }}
+                    resizeMode='contain'
+                    className='ml-[-30]'
+                    />
+                    {/* <Text className='flex items-center justify-center truncate mt-[-30%] text-white font-normal w-[50%] text-xs text-center p-3 border-[1.1px] rounded-full border-white'>
+                        Start by uploading your homework or ask a question.
+                    </Text> */}
+                </View>}
+
+                {isChatActive && <View className='flex-1 items-center justify-center'>
+                    <Text className='text-white'>Chat Active</Text>
+                    </View>
+                }
+
             </View>
-        </KeyboardAvoidingView>
-    </SafeAreaView>
-  )
+
+            <ChatInputSection
+                isChatActive={isChatActive}
+                setIsChatActive={setIsChatActive}
+                onContinue={handleContinue}
+            />
+
+            <ReactNativeModal isVisible={showRequestModal} onBackdropPress={() => setShowRequestModal(false)}>
+                <View className='min-h-[200px] rounded-2xl p-5 bg-white flex'>
+                    <Text className='text-md'>You must grant SmartLearning access to your camera in order to take a photo</Text>
+                    <View className='flex flex-row justify-between mt-5'>
+                        <TouchableOpacity
+                            onPress={() => setShowRequestModal(false)}
+                        >
+                            <Text>Cancel</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={handleCameraPermission}
+                        >
+                            <Text className='font-semibold'>Request Permission</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+            </ReactNativeModal>
+
+        </SafeAreaView>
+    )
 }
 
 export default Homework
